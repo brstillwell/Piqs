8b7d93ecdbe0650aea9e605937f2f97f16c5d193
Brandon <Brandon.Stillwell@gmail.com>
2020-01-15 21:57:34 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Piqs',
+    version='0.1dev',
+    packages=['Piqs',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
